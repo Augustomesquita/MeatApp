8cb4121577192151ec9b37a7e175c3c92fd3d685
Augusto <augusto.mesquita@leucotron.com.br>
2017-10-20 08:13:13 -0200

    Bug em catch de chamada http.

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -17,7 +17,7 @@ export class RestaurantsService {
 
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
-            .map(response => response.json()
-            .catch(ErrorHandler.handleError));
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
     }
 }
