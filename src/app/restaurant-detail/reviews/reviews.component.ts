160c177f407adada30bda90470433709849e1262
Augusto <augusto.mesquita@leucotron.com.br>
2017-10-23 11:54:42 -0200

    Adicionado reviews (template e consumo rest - Utilizado pipes para formatação de dados e para realizar subscribe em 'Observable' de chamadas http.

---
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from '../../restaurants/restaurants.service';
+import { Restaurant } from '../../restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'mt-reviews',
+  templateUrl: './reviews.component.html'
+})
+export class ReviewsComponent implements OnInit {
+
+  reviews: Observable<any>
+
+  constructor(private restaurantService: RestaurantsService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.reviews = this.restaurantService.restaurantReview(this.route
+      .parent.snapshot.params['id']);
+  }
+
+}
